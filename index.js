a88c76e0c0f48ee8e45c0e46fb90ce90f0feb418
Ray Flores <ray.m.flores@gmail.com>
2021-12-05 01:16:01 +0000

    completed transaction history stretch. Leaving final feature for now

--- index.js
+++ index.js
@@ -1,13 +1,53 @@
-let balance = 500.00;
+// let balance = 500.00;
 
-class Withdrawal {
+class Account {
 
-  constructor(amount) {
+  constructor(username) {
+    this.username = username;
+    this.transactions = []; //changed from this.balance = x;
+  }
+
+  get balance() {
+    let balance = 0;
+    for (let t of this.transactions) {
+      balance += t.value;
+    }
+    return balance;
+  }
+
+  addTransaction(transaction) {
+    this.transactions.push(transaction);
+  }
+
+}
+
+class Transaction {
+
+  constructor(amount, account) {
     this.amount = amount;
+    this.account = account;
   }
 
   commit() {
-    balance -= this.amount;
+
+    this.time = new Date();
+    this.account.addTransaction(this);
+  }
+
+}
+
+class Deposit extends Transaction {
+
+  get value() {
+    return this.amount
+  }
+
+}
+
+class Withdrawal extends Transaction {
+
+  get value() {
+    return -this.amount
   }
 
 }
@@ -15,15 +55,30 @@ class Withdrawal {
 
 
 
+
+
+
+
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
 
-t1 = new Withdrawal(50.25);
+const myAccount = new Account("snow-patrol");
+
+t1 = new Withdrawal(50.25, myAccount);
 t1.commit();
 console.log('Transaction 1:', t1);
 
-t2 = new Withdrawal(9.99);
+t2 = new Withdrawal(9.99, myAccount);
 t2.commit();
 console.log('Transaction 2:', t2);
 
-console.log('Balance:', balance);
+console.log('Balance:', myAccount.balance);
+
+t3 = new Deposit(120.00, myAccount);
+t3.commit();
+console.log('Transaction 3:', t3);
+
+console.log('Balance:', myAccount.balance);
+
+
+// Add the other features as STRETCH!
